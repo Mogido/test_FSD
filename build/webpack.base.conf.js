192c708c5b4e484c0b307fd8fe1c001713f43bec
Максим <makstrofi@yandex.ru>
2019-11-27 16:15:13 +0300

    подключил pug

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,8 +4,8 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-
 // Main const
+// see more: https://github.com/vedees/webpack-template/blob/master/README.md#main-const
 const PATHS = {
   src: path.join(__dirname, '../src'),
   dist: path.join(__dirname, '../dist'),
@@ -13,8 +13,10 @@ const PATHS = {
 }
 
 // Pages const for HtmlWebpackPlugin
-const PAGES_DIR = PATHS.src
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.html'))
+// see more: https://github.com/vedees/webpack-template/blob/master/README.md#html-dir-folder
+// const PAGES_DIR = PATHS.src
+const PAGES_DIR = `${PATHS.src}/pug/pages/`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
 
 module.exports = {
   // BASE config
@@ -23,7 +25,7 @@ module.exports = {
   },
   entry: {
     app: PATHS.src,
-    // module: `${PATHS.src}/common.js`,
+    // module: `${PATHS.src}/your-module.js`,
   },
   output: {
     filename: `${PATHS.assets}js/[name].[hash].js`,
@@ -44,6 +46,19 @@ module.exports = {
   },
   module: {
     rules: [{
+      test: /\.pug$/,
+      oneOf: [
+        // this applies to <template lang="pug"> in Vue components
+        {
+          resourceQuery: /^\?vue/,
+          use: ['pug-plain-loader']
+        },
+        // this applies to pug imports inside JavaScript
+        {
+          use: ['pug-loader']
+        }
+      ]
+    }, {
       test: /\.js$/,
       loader: 'babel-loader',
       exclude: '/node_modules/'
@@ -92,7 +107,8 @@ module.exports = {
   },
   resolve: {
     alias: {
-      '~': PATHS.src
+      '~': PATHS.src,
+      'vue$': 'vue/dist/vue.js',
     }
   },
   plugins: [
@@ -106,9 +122,11 @@ module.exports = {
     ]),
 
     // Automatic creation any html pages (Don't forget to RERUN dev server)
+    // see more: https://github.com/vedees/webpack-template/blob/master/README.md#create-another-html-files
+    // best way to create pages: https://github.com/vedees/webpack-template/blob/master/README.md#third-method-best
     ...PAGES.map(page => new HtmlWebpackPlugin({
       template: `${PAGES_DIR}/${page}`,
-      filename: `./${page}`
+      filename: `./${page.replace(/\.pug/,'.html')}`
     }))
   ],
 }
